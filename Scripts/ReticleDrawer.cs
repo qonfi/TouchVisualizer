d9c5b2903554903ae90edc16150b11eefa80e963
qonfi <confidentialrendezvous@gmail.com>
2017-06-21 20:07:25 +0900

    Add files via upload

---
+++ Scripts/ReticleDrawer.cs
@@ -0,0 +1,16 @@
+﻿//using UnityEngine.SceneManagement;
+//using UnityEngine.Networking; // (needs NetworkBehaviour)
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+
+namespace TouchVisualizer
+{
+    public class ReticleDrawer 
+    {
+
+    }
+}
